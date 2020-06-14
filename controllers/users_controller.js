5fc67520c8ed772f6c2c8e0bb9d080eaae7a48cd
Shubham <shubhamnagpal30@gmail.com>
2020-06-14 14:44:54 +0530

    Add and render sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,27 @@
 module.exports.profile = function(req, res){
   return res.end('<h1>User Profile</h1>');
 }
+
+// render sign out page
+module.exports.signUp = function(req, res) {
+  return res.render('user_sign_up', {
+    title: "HabitTracker | sign up"
+  })
+}
+
+// Render sign in page
+module.exports.signIn = function(req, res) {
+  return res.render('user_sign_in', {
+    title: "HabitTracker | sign in"
+  })
+}
+
+// get the sign up data 
+module.exports.create = function(req, res) {
+
+}
+
+// Sign in and create a session for the user
+module.exports.createSession = function(req, res) {
+
+}
--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile', usersController.profile);
 
+router.get('/sign-in', usersController.signIn);
+
+router.get('/sign-up', usersController.signUp);
+
 module.exports = router;
